71338be5221ca8efc6a5084c258176793ba1dcf0
methreban2023 <alrefai.dalia@gmail.com>
2023-07-06 14:32:22 +0300

    adding schemas Ingredient

---
+++ models/Ingredient.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require("mongoose");
+
+const ingredientSchema = new Schema(
+  {
+    name: { type: String, unique: true, requires: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User" },
+    recipies: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Ingredient", ingredientSchema);
