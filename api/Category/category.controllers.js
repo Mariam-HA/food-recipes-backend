3b00ae598bc75181cdf82459d2a8cfa6df66fd06
nouftalajmi <alajminouf94@gmail.com>
2023-07-07 18:14:13 +0300

    added category functions

--- api/Category/category.controllers.js
+++ api/Category/category.controllers.js
@@ -6,7 +6,7 @@ const Category = require("../../models/Category")
 const createCategory = async (req, res, next) => {
     try {
         if (req.file) {
-            req.body.Image = `${req.file.path}`
+            req.body.catImage = `${req.file.path}`
         }
         const category = await Category.create(req.body)
         return res.status(201).json(category)
@@ -31,5 +31,25 @@ const getCategory = async (req, res, next) => {
         next(error)
     }
 }
+const getCatById = async (req, res, next) => {
+    try {
+        const { catigoryId } = req.params
+        const category = await Category.findById(catigoryId)
+        if (category) {
+            res.status(200).json(category)
+        } res.status(404).json({ message: "Category not found" })
+    } catch (error) {
+        next(error)
 
-module.exports = { createCategory, getCategory }
+    }
+}
+const deleteCat = async (req, res, next) => {
+    try {
+        await Category.deleteOne()
+        return res.status(204).end()
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = { createCategory, getCategory, getCatById, deleteCat }
--- api/ingredients/ingredients.routes.js
+++ api/ingredients/ingredients.routes.js
@@ -1,9 +1,9 @@
 express = require('express')
 const passport = require('passport');
 const { getIngredents, createIngredent } = require('./ingredients.controller');
-
+require("dotenv").config()
 const router = express.Router()
 
 router.get("/", getIngredents)
-router.post("/", createIngredent)
+router.post("/", passport.authenticate('jwt', { session: false }), createIngredent)
 module.exports = router
